9e6fd5e8dc1967904c9503e2c15d531bbbf6637c
DJIDJIK Adel <ea_djidjik@esi.dz>
2020-05-04 16:22:11 +0100

    adding authors list to home

--- src/Containers/Home.js
+++ src/Containers/Home.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Layout, Row, Col, Button } from "antd";
 import Icon from "../Components/Icon/Icon";
 import Card from "../Components/Card/Card";
+import Author from "../Components/Author/Author";
+import AuthorCard from "../Components/AuthorCard/AuthorCard";
 
 const { Content } = Layout;
 
@@ -38,15 +40,55 @@ export default function Home() {
       </Row>
       <hr className="line" />
       <Row style={{ marginTop: 16 }}>
-        <Col span={12}>
-          <h3 className="white b">Last updates :</h3>
-          <Card
-           title="Trump gives Corona virus updates alongside White House task Force"
-           date="April 13, 2020"
-           image="news.png"
-           />
+        <Col className="left-col" span={12}>
+          <section className="section">
+            <h3 className="white b">Last updates :</h3>
+            <Card
+              title="Trump gives Corona virus updates alongside White House task Force"
+              date="April 13, 2020"
+              image="news.png"
+            />
+          </section>
+
+          <section className="section">
+            <h3 className="white b">Description</h3>
+            <p className="white">
+              A card can be used to display content related to a single subject.
+              The content can consist of multiple elements of varying types and
+              sizes. A card can be used to display content related to a single
+              subject. The content can consist of multiple elements of varying
+              types and sizes.
+            </p>
+          </section>
         </Col>
-        <Col span={12}></Col>
+        <Col className="right-col" span={12}>
+          <section className="section">
+            <h3 className="white b">Playlist Authors</h3>
+            <div className="list-authors">
+              <Author className="white" name="name1" picture="person1.png" />
+              <Author className="white" name="name2" picture="person2.png" />
+              <Author className="white" name="name1" picture="person3.png" />
+              <Author className="white" name="name1" picture="person4.png" />
+              <Author className="white" name="name1" picture="person5.png" />
+            </div>
+          </section>
+          <section className="section">
+          <h3 className="white b">Hashtags</h3>
+            <div className="list-hashtags">
+              <span className="hashtag">#news</span>
+              <span className="hashtag">#Covid</span>
+              <span className="hashtag">#Bigresponse</span>
+            </div>
+          
+          </section>
+          <section className="section">
+          <h3 className="white b">Video Authors</h3>
+            <div className="list-authors-cards">
+              <AuthorCard  />
+            </div>
+          
+          </section>
+        </Col>
       </Row>
     </Content>
   );
