0805fa87705c112b722f1e868e126237c04517ec
DJIDJIK Adel <ea_djidjik@esi.dz>
2020-05-06 00:31:46 +0100

    caroussel animated !

--- src/Components/Caroussel/Caroussel.js
+++ src/Components/Caroussel/Caroussel.js
@@ -1,4 +1,5 @@
-import React, {useEffect, useState, createRef} from "react";
+import React, { useEffect, useState, createRef } from "react";
+import Carousel from "react-elastic-carousel";
 import ArrowNavigation from "../ArrowNavigation/ArrowNavigation";
 import "./caroussel.min.css";
 
@@ -19,55 +20,56 @@ const Card = ({
   );
 };
 
-const Y_STEP=141;
+const Y_STEP = 141;
 
-export default function Caroussel({list=[]}) {
+export default function Caroussel({ list = [] }) {
+  const [isMobile, setisMobile] = useState(false);
+  const [theNewElement, settheNewElement] = useState(null);
+  const [cardsByPage, setcardsByPage] = useState(4);
+  const [currentLeftIndex, setcurrentLeftIndex] = useState(0);
+  const refDiv = createRef();
+  const Refcarousel = createRef();
 
-    const [isMobile, setisMobile] = useState(false)
-    const [theNewElement, settheNewElement] = useState(null)
-    const [cardsByPage, setcardsByPage] = useState(4)
-    const [currentLeftIndex, setcurrentLeftIndex] = useState(0)
-    const refDiv = createRef();
+  useEffect(() => {
+    updateDimensions();
+    if (isMobile && cardsByPage !== 2) {
+      setcardsByPage(2);
+    } else {
+      setcardsByPage(4);
+    }
+    window.addEventListener("resize", updateDimensions.bind(this));
+    return () => {
+      window.removeEventListener("resize", updateDimensions.bind(this));
+    };
+  });
 
-    useEffect(()=>{
-        
-        updateDimensions()
-        if(isMobile && cardsByPage!==2){
-            setcardsByPage(2)
-        }else{
-            setcardsByPage(4)
-        }
-        window.addEventListener("resize", updateDimensions.bind(this));
-        return(()=>{
-            window.removeEventListener("resize", updateDimensions.bind(this));
-        })
-    })
-
-    
-    const updateDimensions= () => {
-        if(window.innerWidth < 700) {
-            setisMobile(true)
-        } else {
-            setisMobile(false)
-        }
-      }
+  const updateDimensions = () => {
+    if (window.innerWidth < 700) {
+      setisMobile(true);
+    } else {
+      setisMobile(false);
+    }
+  };
 
   const onscroll = () => {
-      console.log("scrolling", refDiv);
-  }
+    console.log("scrolling", refDiv);
+  };
   const moveLeft = () => {
-    console.log("scrolling left ",currentLeftIndex);
-    if(currentLeftIndex>0){
-        refDiv.current.scrollLeft=currentLeftIndex-Y_STEP;
-        setcurrentLeftIndex(currentLeftIndex-Y_STEP)
-    }
-    
-  }
+    Refcarousel.current.slidePrev();
+
+    // console.log("scrolling left ",currentLeftIndex);
+    // if(currentLeftIndex>0){
+    //     refDiv.current.scrollLeft=currentLeftIndex-Y_STEP;
+    //     setcurrentLeftIndex(currentLeftIndex-Y_STEP)
+    // }
+  };
   const moveRight = () => {
-    console.log("scrolling right ",currentLeftIndex);
-    refDiv.current.scrollLeft=currentLeftIndex+Y_STEP;
-    setcurrentLeftIndex(currentLeftIndex+Y_STEP)
-  }
+    Refcarousel.current.slideNext();
+    // console.log("scrolling right ",currentLeftIndex);
+    // refDiv.current.scrollLeft=currentLeftIndex+Y_STEP;
+    // setcurrentLeftIndex(currentLeftIndex+Y_STEP)
+  };
+
   return (
     <div className="caroussel-container">
       <div className="caroussel-navigation">
@@ -79,24 +81,34 @@ export default function Caroussel({list=[]}) {
         </span>
       </div>
       <hr className="hr" />
-      
-        {isMobile ? (
-            <div className="caroussel-list-cards mini">
-            <Card duration="18:40" />
-            <Card duration="10:11" />
-          </div>
-        ) : (
-            <div ref={refDiv} onScroll={onscroll} className="caroussel-list-cards">
-            <Card duration="09:22" />
-            <Card duration="09:22" />
-            <Card duration="09:22" />
-            <Card duration="09:22" />
-            <Card duration="09:22" />
-            <Card duration="09:22" />
-            <Card duration="09:22" />
-            <Card duration="09:22" />
-          </div>
-        )}
+      <Carousel ref={Refcarousel} itemsToShow={3} showArrows={false} pagination={false}>
+        <Card duration="09:22" />
+        <Card duration="09:22" />
+        <Card duration="09:22" />
+        <Card duration="09:22" />
+        <Card duration="09:22" />
+        <Card duration="09:22" />
+        <Card duration="09:22" />
+      </Carousel>
+      {
+        //     isMobile ? (
+        //     <div className="caroussel-list-cards mini">
+        //     <Card duration="18:40" />
+        //     <Card duration="10:11" />
+        //   </div>
+        // ) : (
+        //     <div ref={refDiv} onScroll={onscroll} className="caroussel-list-cards">
+        //     <Card duration="09:22" />
+        //     <Card duration="09:22" />
+        //     <Card duration="09:22" />
+        //     <Card duration="09:22" />
+        //     <Card duration="09:22" />
+        //     <Card duration="09:22" />
+        //     <Card duration="09:22" />
+        //     <Card duration="09:22" />
+        //   </div>
+        // )
+      }
     </div>
   );
 }
